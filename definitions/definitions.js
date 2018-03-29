eb250e3f3f8b5d44b5159f512dc21cfad1c67f7a
Yulong Tan <yulongtan67@gmail.com>
2018-03-29 10:52:01 -0700

    initial definitions file

---
+++ definitions/definitions.js
@@ -0,0 +1,8 @@
+/*
+ * Definitions takes a line-by-line text file of terms in the format of 
+ * term: definition
+ */
+
+const fs = require('fs'); // Read and write files
+const readline = require('readline') // Reads files line by like
+
