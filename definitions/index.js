ae062ee459d058b2a37768452ad1afe97e6459c5
Yulong Tan <yulongtan67@gmail.com>
2018-03-29 12:19:50 -0700

    complete definitions script

--- definitions/index.js
+++ definitions/index.js
@@ -3,6 +3,36 @@
  * term: definition
  */
 
-const fs = require('fs'); // Read and write files
-const readline = require('readline') // Reads files line by like
+const fs = require('fs');            // Read and write files
+
+const inFile = 'definitions.txt'
+const outFile = 'definitions-sorted.txt';
+
+var terms = [];
+var results = '';
+
+fs.readFile(inFile, function(err, data) {
+	if (err) {
+		throw err;
+	}
+	terms = data.toString().split('\n');
+	terms.sort();
+	results = toFileFormat(terms);
+	
+	fs.writeFile(outFile, results, 'utf8', function() {
+		console.log(`Results saved to ${outFile}`);
+	});
+});
+
+function toFileFormat(arr) {
+	var result = '';
+	if (arr.length > 0) {
+		result = arr[0].trim() + '\n';
+		for (var i = 1; i < arr.length; i++) {
+			result += arr[i].trim() + '\n';
+		}
+	}
+	return result;
+}
+
 
